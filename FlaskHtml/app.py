c55b755f768c70b5d958bb73d1c2058f31a42d70
Omar Loudghiri <62630012+OmarLoud1@users.noreply.github.com>
2024-03-02 14:22:53 -0500

    flask app

--- FlaskHtml/app.py
+++ FlaskHtml/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 from collections import defaultdict
 
@@ -16,20 +16,20 @@ def index():
         domain_as = lb_info.get("domain_info", "Unknown AS")
         for lb_ip, as_info in lb_info.items():
             if lb_ip != "domain_info" and as_info != "UCB, US":  # Skip 'domain_info' and 'UCB, US'
-                lb_groups[(lb_ip, as_info)][domain_as].add(domain)
+                lb_groups[lb_ip][domain_as].add(domain)
 
-    # Flatten the groups to a list of (count, lb_ip, as_info, domain_as, domains) tuples
+    # Flatten the groups to a list of (count, lb_ip, domain_as, domains) tuples
     flat_groups = []
-    for (lb_ip, as_info), domain_groups in lb_groups.items():
+    for lb_ip, domain_groups in lb_groups.items():
         for domain_as, domains in domain_groups.items():
-            flat_groups.append((len(domains), lb_ip, as_info, domain_as, ', '.join(domains)))
+            flat_groups.append((len(domains), lb_ip, domain_as, ', '.join(domains)))
 
     # Sort the list by count (descending)
     sorted_groups = sorted(flat_groups, key=lambda x: x[0], reverse=True)
 
     # Convert processed data to JSON format
-    json_data = [{'count': count, 'lb_ip': lb_ip, 'as_info': as_info, 'domain_as': domain_as, 'domains': domains} 
-                 for count, lb_ip, as_info, domain_as, domains in sorted_groups[:20]]
+    json_data = [{'count': count, 'lb_ip': lb_ip, 'domain_as': domain_as, 'domains': domains} 
+                 for count, lb_ip, domain_as, domains in sorted_groups]
 
     return render_template('index.html', json_data=json_data)
 
