36b3220ddc6a0d310c8f360a18a2f150bb015306
Ari <ariostrow@gmail.com>
2024-04-28 09:57:47 -0700

    validated user input - version2

--- Scripts/parallel.sh
+++ Scripts/parallel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 X"
+    echo "Usage: $0 X (where X is a positive integer)"
     exit 1
 fi
 
-X=($1-1)
+if ! [[ $1 =~ ^[1-9][0-9]*$ ]]; then
+    echo "Error: X must be a positive integer."
+    exit 1
+fi
+
+X=$(( $1 - 1 ))
 for ((i=0; i<=X; i++)); do
     sudo python3 handshake.py demo &
 done
